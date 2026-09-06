b2dd820aac5900430916556717261a25e6f72036
Davi Oliveira <davisouza59@outlook.com>
2026-09-05 23:27:02 -0300

    correcao

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from botocore.exceptions import ClientError, NoCredentialsError
 from dotenv import load_dotenv
 from flask import Flask, jsonify
 
-
 # Configura o logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
