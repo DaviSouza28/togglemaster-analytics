b570e1c6ef422d64de02591c23bb66f2942a8152
Davi Oliveira <davisouza59@outlook.com>
2026-09-05 23:15:39 -0300

    test: demonstrate security pipeline failure

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import boto3
 from botocore.exceptions import ClientError, NoCredentialsError
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+import subprocess
+
+subprocess.Popen("echo teste", shell=True)
 
 # Configura o logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
